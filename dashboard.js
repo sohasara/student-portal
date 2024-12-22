8e0fcebc3bc42c00d4cf5497175bf9a92cb6ca49
kawser-miah <kawsermiah658@gmail.com>
2024-12-22 21:20:36 +0600

    bill option add

--- dashboard.js
+++ dashboard.js
@@ -20,12 +20,51 @@ function run() {
             // alert(userObject.first_name);
             const spanElement = document.getElementById("name");
             spanElement.textContent = userObject.first_name;
+            var data ={ stu_id:userObject.stu_id};
+
+            fetch('payments.php',
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data), // Send stu_id as JSON
+        })
+                .then(response => {
+                    if (!response.ok) {
+                        return response.json().then(errorData => {
+                            // Show the error message in an alert box
+                            // alert('Error: ' + errorData.error);
+                            // Throw an error that will be caught in the catch block
+                            throw new Error(errorData.error);  // This will trigger the catch block
+                        });
+                    }
+                    return response.json();  // Return the data if successful
+                })
+                .then(data => {
+                    
+                    data.forEach(user => {
+                        const elements = document.getElementsByClassName("p1");
+                        elements[0].textContent= user.total_bill;
+                        elements[1].textContent = user.total_paid;
+                        elements[2].textContent = user.total_bill - user.total_paid;
+                        localStorage.setItem("hi",JSON.stringify(user));
+                        console.log(user.total_fee);
+                        
+                    }
+
+                    );
+           
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error)
+                    alert(error);
+                });
         } else {
             const spanElement = document.getElementById("name");
             spanElement.textContent = "No Name";
 
 
         }
+
     }
 
     // Call the function
